c31e4cd7e90e7bd954985497fd482f1f774cc8f3
Derrick Adkins <derrick.l.adkins@gmail.com>
2023-05-18 14:51:25 -0400

    Update Skill.kt

--- app/src/main/java/com/locoquest/app/Skill.kt
+++ app/src/main/java/com/locoquest/app/Skill.kt
@@ -2,7 +2,7 @@ package com.locoquest.app
 
 enum class Skill(val text: String, val effect: Int, val duration: Int, val reuseIn: Int) {
     DRONE("Drone", 0, 300, 300),
-    COMPANION("Companion", 0, 300, 300),
+    COMPANION("Companion", 10, 300, 300),
     TIME("Time", 14400,0,  300),
     GIANT("Giant", 300, 300, 300)
 }
