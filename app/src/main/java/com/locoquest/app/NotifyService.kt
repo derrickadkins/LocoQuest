65a2541cfce60ddbd58f71656f34ac11e3cd1df8
Derrick Adkins <derrick.l.adkins@gmail.com>
2023-05-18 08:32:29 -0400

    Update NotifyService.kt

--- app/src/main/java/com/locoquest/app/NotifyService.kt
+++ app/src/main/java/com/locoquest/app/NotifyService.kt
@@ -21,48 +21,54 @@ class NotifyService: JobService() {
 
     override fun onStartJob(p0: JobParameters?): Boolean {
         Log.d(TAG, "onStartCommand:$p0")
+        if(p0 == null) return false
+        if(p0.extras.size() == 0) return false
 
         val config = Configuration.Builder()
         config.setJobSchedulerJobIdRange(0, 1000)
 
-        val id = p0?.extras?.getString("pid", "")
-        val name = p0?.extras?.getString("name", "")
-        val contentText = if (name == null) "Coin is available to be collected again"
-        else "Coin ($name) is available to be collected again"
+        if(p0.extras.containsKey("pid")) {
+            val id = p0.extras.getString("pid", "")
+            val name = p0.extras.getString("name", "")
+            val contentText = if (name == null) "Coin is available to be collected again"
+            else "Coin ($name) is available to be collected again"
 
-        Log.d("notify receiver", "intent received for $name")
+            Log.d("notify receiver", "intent received for $name")
 
-        createNotificationChannel()
-        notificationManagerCompat = NotificationManagerCompat.from(this)
+            createNotificationChannel(channels[0])
+            notificationManagerCompat = NotificationManagerCompat.from(this)
 
-        val contentIntent = Intent(this, MainActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(
-            this,
-            id.hashCode(),
-            contentIntent,
-            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-        )
-
-        builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.locoquest_notification_icon)
-            .setLargeIcon(
-                BitmapFactory.decodeResource(
-                    this.resources,
-                    R.drawable.coin
-                )
+            val contentIntent = Intent(this, MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(
+                this,
+                id.hashCode(),
+                contentIntent,
+                PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
             )
-            .setPriority(NotificationCompat.PRIORITY_MAX)
-            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-            .setLights(this.getColor(R.color.blue), 1000, 1000)
-            .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
-            .setAutoCancel(true)
-            .setContentIntent(pendingIntent)
-            .setContentTitle("LocoQuest")
-            .setContentText(contentText)
 
-        val notification = builder!!.build()
-        notification.flags = notification.flags or Notification.FLAG_SHOW_LIGHTS
-        notificationManagerCompat!!.notify(id.hashCode(), notification)
+            builder = NotificationCompat.Builder(this, channels[0])
+                .setSmallIcon(R.drawable.locoquest_notification_icon)
+                .setLargeIcon(
+                    BitmapFactory.decodeResource(
+                        this.resources,
+                        R.drawable.coin
+                    )
+                )
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                .setLights(this.getColor(R.color.blue), 1000, 1000)
+                .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .setContentTitle("LocoQuest")
+                .setContentText(contentText)
+
+            val notification = builder!!.build()
+            notification.flags = notification.flags or Notification.FLAG_SHOW_LIGHTS
+            notificationManagerCompat!!.notify(id.hashCode(), notification)
+        }else if (p0.extras.containsKey("ordinal")){
+            
+        }
 
         return true
     }
@@ -71,14 +77,14 @@ class NotifyService: JobService() {
         return false
     }
 
-    private fun createNotificationChannel() {
+    private fun createNotificationChannel(channelInfo: String) {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel =
-                NotificationChannelCompat.Builder(CHANNEL_ID, NotificationManager.IMPORTANCE_HIGH)
-                    .setName(this.getString(R.string.channel_name))
-                    .setDescription(this.getString(R.string.channel_description)) //.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC)
+                NotificationChannelCompat.Builder(channelInfo, NotificationManager.IMPORTANCE_HIGH)
+                    .setName(channelInfo)
+                    .setDescription(channelInfo) //.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC)
                     .setVibrationEnabled(true)
                     .setLightsEnabled(true)
                     .setVibrationPattern(longArrayOf(1000, 1000, 1000, 1000, 1000))
@@ -92,6 +98,6 @@ class NotifyService: JobService() {
 
     companion object{
         private const val TAG = "NotifyService"
-        private const val CHANNEL_ID = "LocoQuest.Coin"
+        private val channels = arrayOf("Coin", "Skill")
     }
 }
