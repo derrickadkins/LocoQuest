2292ba5eaa6544534586f0c4f4e296b2813dfe99
Derrick Adkins <derrick.l.adkins@gmail.com>
2023-05-19 07:49:27 -0400

    Update Upgrade.kt

--- app/src/main/java/com/locoquest/app/Upgrade.kt
+++ app/src/main/java/com/locoquest/app/Upgrade.kt
@@ -1,14 +1,14 @@
 package com.locoquest.app
 
 enum class Upgrade(val skill: Skill, name: String, val effect: Int) {
-    DRONE_BATT(Skill.DRONE, "Battery", 300),
-    DRONE_CHARGE(Skill.DRONE, "Charger", -300),
+    DRONE_BATT(Skill.DRONE, "Battery", 30),
+    DRONE_CHARGE(Skill.DRONE, "Charge", -1800),
     DRONE_MOTOR(Skill.DRONE, "Motors", 5),
-    COMPANION_BATT(Skill.COMPANION, "Battery", 300),
-    COMPANION_CHARGE(Skill.COMPANION, "Charger", -300),
+    COMPANION_BATT(Skill.COMPANION, "Battery", 300), //todo - remove?
+    COMPANION_CHARGE(Skill.COMPANION, "Charge", -1800),
     COMPANION_MOTOR(Skill.COMPANION, "Motors", 5),
     GIANT_BATT(Skill.GIANT, "Battery", 300),
-    GIANT_CHARGE(Skill.GIANT, "Charger", -300),
-    GIANT_REACH(Skill.GIANT, "Reach", 300),
-    TIME_CHARGE(Skill.TIME, "Charge", -300)
+    GIANT_CHARGE(Skill.GIANT, "Charge", -300),
+    GIANT_REACH(Skill.GIANT, "Reach", 100),
+    TIME_CHARGE(Skill.TIME, "Charge", -7200)
 }
