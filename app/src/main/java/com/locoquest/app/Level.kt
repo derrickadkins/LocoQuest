6105c0d4d096803fc70be9f9749f723f422fd169
Derrick Adkins <derrick.l.adkins@gmail.com>
2023-05-30 08:22:51 -0400

    Update Level.kt

--- app/src/main/java/com/locoquest/app/Level.kt
+++ app/src/main/java/com/locoquest/app/Level.kt
@@ -2,15 +2,20 @@ package com.locoquest.app
 
 enum class Level(val level: Long, val upperLimit: Long) {
     LEVEL_1(1, 100),
-    LEVEL_2(2, 200),
-    LEVEL_3(3, 300),
-    LEVEL_4(4, 400),
-    LEVEL_5(5, 500),
-    LEVEL_6(6, 600),
-    LEVEL_7(7, 700),
-    LEVEL_8(8, 800),
-    LEVEL_9(9, 900),
-    LEVEL_10(10, 1000);
+    LEVEL_2(2, 300),
+    LEVEL_3(3, 600),
+    LEVEL_4(4, 1000),
+    LEVEL_5(5, 1500),
+    LEVEL_6(6, 2100),
+    LEVEL_7(7, 2800),
+    LEVEL_8(8, 3600),
+    LEVEL_9(9, 4500),
+    LEVEL_10(10, 5500),
+    LEVEL_11(11, 6600),
+    LEVEL_12(12, 7800),
+    LEVEL_13(13, 9100),
+    LEVEL_14(14, 10500),
+    LEVEL_15(15, 12000);
 
     companion object {
         fun getLimits(levelNumber: Long): Pair<Long, Long> {
